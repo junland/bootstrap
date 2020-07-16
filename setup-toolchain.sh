d128d6c40aa40e49dead5db4ed44fbec23529774
John Unland <unlandj2012@gmail.com>
2020-07-16 10:47:47 -0500

    Create setup-toolchain.sh

---
+++ setup-toolchain.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -e
+
+CWD=$(pwd)
+umask 022
+
+if [ "$1" == "" ]; then
+  echo " >>> Please specify a architecture to build."
+  exit 1
+fi
+
+TOOLCHAIN_ARCH=$1
+TOOLCHAIN_URL=https://toolchains.bootlin.com/downloads/releases/toolchains/$TOOLCHAIN_ARCH/tarballs/$TOOLCHAIN_ARCH--glibc--bleeding-edge-2020.02-2.tar.bz2
+TOOLCHAIN_DIR=$CROSS_ARCH--glibc--bleeding-edge-2020.02-2
+
+echo " >>> Creating rootfs dir..."
+
+mkdir -vp $CWD/rootfs
+
+echo " >>> Getting toolchain..."
+
+wget $TOOLCHAIN_URL
+
+echo " >>> Unpacking toolchain..."
+
+tar -xvf ./$TOOLCHAIN_ARCH--glibc--bleeding-edge-2020.02-2.tar.bz2 && rm ./$TOOLCHAIN_ARCH--glibc--bleeding-edge-2020.02-2.tar.bz2
+
+echo " >>> Relocate toolchain..."
+
+. $CWD/$TOOLCHAIN_ARCH--glibc--bleeding-edge-2020.02-2/relocate-sdk.sh
+
+echo " >>> Done setting up toolchain..."
