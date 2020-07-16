163a8d7c6deb8a472dad4d26ac8d7bd4084907cf
John Unland <unlandj2012@gmail.com>
2020-07-16 10:49:59 -0500

    Update setup-toolchain.sh

--- setup-toolchain.sh
+++ setup-toolchain.sh
@@ -10,7 +10,6 @@ fi
 
 TOOLCHAIN_ARCH=$1
 TOOLCHAIN_URL=https://toolchains.bootlin.com/downloads/releases/toolchains/$TOOLCHAIN_ARCH/tarballs/$TOOLCHAIN_ARCH--glibc--bleeding-edge-2020.02-2.tar.bz2
-TOOLCHAIN_DIR=$CROSS_ARCH--glibc--bleeding-edge-2020.02-2
 
 echo " >>> Creating rootfs dir..."
 
