86fffd0898f85d03bbffebca9658af63ad93e639
John Unland <unlandj2012@gmail.com>
2020-07-16 12:04:47 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -31,11 +31,13 @@ case "$TOOLCHAIN_ARCH" in
     export XHOST="$(echo ${MACHTYPE} | sed -e 's/-[^-]*/-cross/')"
     export XTARGET="x86_64-buildroot-linux-gnu"
     export KARCH="x86_64"
+    export GCC_FLAGS="--with-arch=x86-64 --with-tune=generic"
     ;;
   aarch64)
     export XHOST="$(echo ${MACHTYPE} | sed -e 's/-[^-]*/-cross/')"
     export XTARGET="aarch64-buildroot-linux-gnu"
     export KARCH="arm64"
+    export GCC_FLAGS="-with-arch=armv8-a --enable-fix-cortex-a53-835769 --enable-fix-cortex-a53-843419"
     ;;
   *)
     msg "Architecture is not set or is not supported by 'bootstrap' script"
@@ -49,13 +51,13 @@ msg "Set kernal arch to: $KARCH"
 
 msg "Download Linux..."
 
-wget https://www.kernel.org/pub/linux/kernel/v5.x/linux-5.7.8.tar.xz
+wget -q https://www.kernel.org/pub/linux/kernel/v5.x/linux-5.7.8.tar.xz
 
 msg "Unpack Linux..."
 
-tar -xvf linux-5.7.8.tar.xz
+tar -xf linux-5.7.8.tar.xz
 
-msg "Install Linux..."
+msg "Build and Install Linux..."
 
 cd linux-5.7.8
 
@@ -68,3 +70,52 @@ find usr/include -name '.*' -delete && rm usr/include/Makefile
 cp -rv usr/include $ROOTFS_DIR/usr
 
 cp -rv usr/include $ROOTFS_DIR/tools
+
+cd ..
+
+msg "Download GCC..."
+
+wget -q http://ftp.gnu.org/gnu/gcc/gcc-10.1.0/gcc-10.1.0.tar.xz
+
+msg "Unpack GCC..."
+
+tar -xf gcc-10.1.0.tar.xz
+
+msg "Build and Install GCC..."
+
+cd gcc-10.1.0
+
+mkdir build
+
+contrib/download_prerequisites
+
+echo -en '\n#undef STANDARD_STARTFILE_PREFIX_1\n#define STANDARD_STARTFILE_PREFIX_1 "/tools/lib/"\n' >> gcc/config/linux.h
+
+echo -en '\n#undef STANDARD_STARTFILE_PREFIX_2\n#define STANDARD_STARTFILE_PREFIX_2 ""\n' >> gcc/config/linux.h
+
+cp -v gcc/Makefile.in{,.orig}
+
+sed 's@\./fixinc\.sh@-c true@' gcc/Makefile.in.orig > gcc/Makefile.in
+
+sed -i '/lp64=/s/lib64/lib/' gcc/config/aarch64/t-aarch64-linux
+
+sed -i '/m64=/s/lib64/lib/' gcc/config/i386/t-linux64
+
+cd build
+
+../gcc-10.1.0/configure \
+    --prefix=/tools \
+    --libdir=/tools/lib64 \
+    --build=${XHOST} \
+    --host=${XTARGET} \
+    --target=${XTARGET} \
+    --with-local-prefix=/tools \
+    --enable-languages=c,c++ \
+    --with-system-zlib \
+    --with-native-system-header-dir=/tools/include \
+    --disable-libssp \
+    --enable-install-libiberty
+    
+make AS_FOR_TARGET="${XTARGET}-as" LD_FOR_TARGET="${XTARGET}-ld"
+
+make install
