74a09bb074e136cbc55f8635556c35004bdf828b
John Unland <unlandj2012@gmail.com>
2020-07-19 17:29:53 -0500

    fix again

--- lib/functions.sh
+++ lib/functions.sh
@@ -9,19 +9,19 @@ run_stage() {
 
     export stage_target
     
-    if ! test -f $CWD/$stage_target; then
+    if ! test -f $STRAP_CWD/$stage_target; then
       msg "Stage script does not exist: $stage_target"
       exit 1
     fi
 
-    if test -f $CWD/progress/$stage_target.done; then
+    if test -f $STRAP_CWD/progress/$stage_target.done; then
       msg "Stage $stage_target has already been completed, skipping..."
       return
     fi
 
-    chmod +x $CWD/$stage_target
+    chmod +x $STRAP_CWD/$stage_target
 
-    source $CWD/bootstrap.env
+    source $STRAP_CWD/bootstrap.env
 
     msg "Running stage: $stage_target"
 
@@ -29,11 +29,11 @@ run_stage() {
 
     stg_name=$(echo ${stage_target} | sed 's/\//\-/g')
 
-    . $CWD/bootstrap.env 2>&1 | tee $CWD/progress/$stg_name.log
+    . $STRAP_CWD/bootstrap.env 2>&1 | tee $STRAP_CWD/progress/$stg_name.log
 
     msg "Done with stage: $stage_target"
 
-    touch $CWD/progress/$stg_name.done
+    touch $STRAP_CWD/progress/$stg_name.done
 }
 
 export -f run_stage
